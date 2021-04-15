00dbf0beadbe37aa8d68663b5e0415ffaf28db23
Petri Lehtinen <petri@digip.org>
2021-04-15 18:08:23 +0300

    Floatind indexed

---
+++ src/floating/indexed.tspec.ts
@@ -0,0 +1,23 @@
+import { expectType } from './test-utils.tspec.js'
+import * as O from '.'
+import { ArrayExpected, ArrayOfIndexValuePairsExpected } from './indexed.js'
+
+const optic = O.indexed
+
+describe('indexed', () => {
+  it('get - not an array', () => {
+    const result = O.get(optic, true)
+    expectType<ArrayExpected<boolean>>()(result)()
+
+    const result2 = O.get(optic, null)
+    expectType<ArrayExpected<null>>()(result2)()
+
+    const result3 = O.get(optic, undefined)
+    expectType<ArrayExpected<undefined>>()(result3)()
+  })
+
+  it('set - not an array of index-value pairs', () => {
+    const result = O.set(optic, 'foo', [])
+    expectType<ArrayOfIndexValuePairsExpected<string>>()(result)()
+  })
+})
