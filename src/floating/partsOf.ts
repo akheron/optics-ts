1f05c386bf7563cebb6de085c62c0d773ebb7f2c
Petri Lehtinen <petri@digip.org>
2021-05-22 21:53:08 +0300

    Add compose-like signature for floating partsOf

--- src/floating/partsOf.ts
+++ src/floating/partsOf.ts
@@ -1,10 +1,15 @@
 import type { Optic, OpticError, TryA, TryT, A, B, S, T, Try } from './optic.js'
 import * as I from '../internals.js'
 import { Apply, Apply2 } from '../hkt.js'
+import { Compose, compose, ComposeArg } from './compose.js'
 
 export interface ArrayExpected<T> extends OpticError {
   readonly _: unique symbol
   readonly _t: T
+}
+
+export interface TraversalExpected extends OpticError {
+  readonly _: unique symbol
 }
 
 interface PartsOfA<Traversal extends Optic<'Traversal', any, any>> extends A {
@@ -28,6 +33,17 @@ interface PartsOfT<Traversal extends Optic<'Traversal', any, any>> extends T {
   >
 }
 
-export const partsOf: <Traversal extends Optic<'Traversal', any, any>>(
+export function partsOf<Traversal extends Optic<'Traversal', any, any>>(
   traversal: Traversal
-) => Optic<'Lens', PartsOfA<Traversal>, PartsOfT<Traversal>> = I.partsOf as any
+): Optic<'Lens', PartsOfA<Traversal>, PartsOfT<Traversal>>
+export function partsOf<Args extends [ComposeArg, ComposeArg, ...ComposeArg[]]>(
+  ...args: Args
+): Compose<Args> extends infer O
+  ? O extends Optic<'Traversal', any, any>
+    ? Optic<'Lens', PartsOfA<O>, PartsOfT<O>>
+    : TraversalExpected
+  : never
+export function partsOf(arg: any, ...args: any[]) {
+  if (!args.length) return I.partsOf(arg)
+  return I.partsOf(compose(arg, ...args) as any) as any
+}
